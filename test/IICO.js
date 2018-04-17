bd8265171d0ab29ef2c2425dfa8186f63c385afc
Clément Lesaege <clemage@live.fr>
2018-04-17 23:46:15 +0100

    Package gestion for truffle test

---
+++ test/IICO.js
@@ -0,0 +1,8 @@
+ /* eslint-disable no-undef */ // Avoid the linter considering truffle elements as undef.
+const { expectThrow, increaseTime } = require('kleros-interaction/helpers/utils')
+const Token = artifacts.require('zeppelin-solidity/MintableToken.sol')
+const IICO = artifacts.require('IICO.sol')
+
+contract('IICO', function (accounts) {
+    
+})
