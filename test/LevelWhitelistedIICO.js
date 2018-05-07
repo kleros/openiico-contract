e67e64753d934c464356558848a61db20c7892b6
astralship genesis <mstefanow@gmail.com>
2018-05-07 15:21:45 +0100

    More tests

--- test/LevelWhitelistedIICO.js
+++ test/LevelWhitelistedIICO.js
@@ -40,12 +40,20 @@ contract('IICO', function (accounts) {
     assert.equal(await lwiico.whitelister.call(), whitelister2, 'Whitelister is not changed')
   })    
 
-  it('Should not be able to set and change whitelister (anyone else)', async () => {
+  it('Should not be able to set whitelister (anyone else)', async () => {
     let startTestTime = web3.eth.getBlock('latest').timestamp
     let lwiico = await LWIICO.new(startTestTime+timeBeforeStart,fullBonusLength,partialWithdrawalLength, withdrawalLockUpLength,maxBonus,beneficiary,maximumBaseContribution,{from: owner})
 
     await expectThrow(lwiico.setWhitelister(whitelister,{from: buyerA}));
-  })    
+  }) 
+
+  it('Should not be able to add to whitelist (anyone else)', async () => {
+    let startTestTime = web3.eth.getBlock('latest').timestamp
+    let lwiico = await LWIICO.new(startTestTime+timeBeforeStart,fullBonusLength,partialWithdrawalLength, withdrawalLockUpLength,maxBonus,beneficiary,maximumBaseContribution,{from: owner})
+
+    await lwiico.setWhitelister(whitelister,{from: owner});
+    await expectThrow(lwiico.addBaseWhitelist([buyerA],{from: buyerA}));
+  })         
 
   it('Should be forbidden to send ETH without whitelist', async () => {
     let startTestTime = web3.eth.getBlock('latest').timestamp
@@ -64,12 +72,12 @@ contract('IICO', function (accounts) {
     await lwiico.addBaseWhitelist([buyerA],{from: whitelister}) 
 
     increaseTime(1010) // time of the crowdasle
-
     lwiico.searchAndBid(1E18, 0, {from: buyerA, value:0.1E18});
-
+    increaseTime(100) // without it the test fails, might be some timing / async issues
     var bid = await lwiico.bids.call(1);
+
     assert.equal(bid[5], buyerA, "Bid is not properly saved");
-  })    
+  })   
 
   it('Should not be possible to send too much ETH after whitelisting', async () => {
     let startTestTime = web3.eth.getBlock('latest').timestamp
@@ -80,7 +88,7 @@ contract('IICO', function (accounts) {
 
     increaseTime(1010) // time of the crowdasle
 
-    await expectThrow(lwiico.searchAndBid(1E18, 0, {from: buyerA, value: maximumBaseContribution+1}));
+    await expectThrow(lwiico.searchAndBid(1E18, 0, {from: buyerA, value: maximumBaseContribution + 1e18})); // the way how JavaScript handles big numbers, cannot just do +1
   }) 
 
   it('Should not be possible to send too much ETH after whitelisting in multiple goes', async () => {
@@ -117,8 +125,104 @@ contract('IICO', function (accounts) {
     await expectThrow(lwiico.searchAndBid(1E18, 0, {from: buyerA, value: 1E18}));
   })    
 
+  it('Should be possible to add and remove multiple users at once to base whitelist', async () => {
+    let startTestTime = web3.eth.getBlock('latest').timestamp
+    let lwiico = await LWIICO.new(startTestTime+timeBeforeStart,fullBonusLength,partialWithdrawalLength, withdrawalLockUpLength,maxBonus,beneficiary,maximumBaseContribution,{from: owner})
 
+    await lwiico.setWhitelister(whitelister,{from: owner});
+    await lwiico.addBaseWhitelist([buyerA, buyerB],{from: whitelister}) 
 
+    increaseTime(1010) // time of the crowdasle
+    lwiico.searchAndBid(1E18, 0, {from: buyerA, value:0.1E18});
+    increaseTime(100)
+    lwiico.searchAndBid(1E18, 0, {from: buyerB, value:0.2E18});
+    var bidA = await lwiico.bids.call(1);
+    var bidB = await lwiico.bids.call(2);
 
+    assert.equal(bidA[5], buyerA, "Bid of buyerA is not properly saved");
+    assert.equal(bidB[5], buyerB, "Bid of buyerB is not properly saved");
 
+    await lwiico.removeBaseWhitelist([buyerA, buyerB],{from: whitelister}) 
+
+    increaseTime(100)
+
+    await expectThrow(lwiico.searchAndBid(1E18, 0, {from: buyerA, value:0.1E18}));
+    await expectThrow(lwiico.searchAndBid(1E18, 0, {from: buyerB, value:0.2E18}));
+  })    
+
+  ////////////// REINFORCED
+
+  it('Should be able to add to reinforced (only whitelister)', async () => {
+    let startTestTime = web3.eth.getBlock('latest').timestamp
+    let lwiico = await LWIICO.new(startTestTime+timeBeforeStart,fullBonusLength,partialWithdrawalLength, withdrawalLockUpLength,maxBonus,beneficiary,maximumBaseContribution,{from: owner})
+
+    await lwiico.setWhitelister(whitelister,{from: owner});
+    await lwiico.addReinforcedWhitelist([buyerA],{from: whitelister});
+  })   
+
+  it('Should not be able to add to reinforced (anyone else)', async () => {
+    let startTestTime = web3.eth.getBlock('latest').timestamp
+    let lwiico = await LWIICO.new(startTestTime+timeBeforeStart,fullBonusLength,partialWithdrawalLength, withdrawalLockUpLength,maxBonus,beneficiary,maximumBaseContribution,{from: owner})
+
+    await lwiico.setWhitelister(whitelister,{from: owner});
+    await expectThrow(lwiico.addReinforcedWhitelist([buyerA],{from: buyerA}));
+  }) 
+
+  it('Should be possible to send a lot ETH after reinforced whitelisting', async () => {
+    let startTestTime = web3.eth.getBlock('latest').timestamp
+    let lwiico = await LWIICO.new(startTestTime+timeBeforeStart,fullBonusLength,partialWithdrawalLength, withdrawalLockUpLength,maxBonus,beneficiary,maximumBaseContribution,{from: owner})
+
+    await lwiico.setWhitelister(whitelister,{from: owner});
+    await lwiico.addReinforcedWhitelist([buyerA],{from: whitelister}) 
+
+    increaseTime(1010) // time of the crowdasle
+
+    lwiico.searchAndBid(1E18, 0, {from: buyerA, value: maximumBaseContribution * 10}); // the way how JavaScript handles big numbers, cannot just do +1
+  }) 
+
+  it('Should be possible to send some ETH first and more after reinforced whitelisting', async () => {
+    let startTestTime = web3.eth.getBlock('latest').timestamp
+    let lwiico = await LWIICO.new(startTestTime+timeBeforeStart,fullBonusLength,partialWithdrawalLength, withdrawalLockUpLength,maxBonus,beneficiary,maximumBaseContribution,{from: owner})
+
+    await lwiico.setWhitelister(whitelister,{from: owner});
+    await lwiico.addBaseWhitelist([buyerA],{from: whitelister}) 
+
+    increaseTime(1010) // time of the crowdasle
+
+    lwiico.searchAndBid(1E18, 0, {from: buyerA, value: 1E18}); // the way how JavaScript handles big numbers, cannot just do +1
+
+    await lwiico.addReinforcedWhitelist([buyerA],{from: whitelister});
+
+    lwiico.searchAndBid(1E18, 0, {from: buyerA, value: 6E18}); // the way how JavaScript handles big numbers, cannot just do +1
+
+    let buyerAContrib =  await lwiico.totalContrib.call(buyerA);
+
+    assert.equal(buyerAContrib, 7E18);
+  }) 
+
+  it('Should be possible to add and remove multiple users at once to reinforced whitelist', async () => {
+    let startTestTime = web3.eth.getBlock('latest').timestamp
+    let lwiico = await LWIICO.new(startTestTime+timeBeforeStart,fullBonusLength,partialWithdrawalLength, withdrawalLockUpLength,maxBonus,beneficiary,maximumBaseContribution,{from: owner})
+
+    await lwiico.setWhitelister(whitelister,{from: owner});
+    await lwiico.addReinforcedWhitelist([buyerA, buyerB],{from: whitelister}) 
+
+    increaseTime(1010) // time of the crowdasle
+    lwiico.searchAndBid(1E18, 0, {from: buyerA, value:6E18});
+    increaseTime(100)
+    lwiico.searchAndBid(1E18, 0, {from: buyerB, value:7E18});
+    var bidA = await lwiico.bids.call(1);
+    var bidB = await lwiico.bids.call(2);
+
+    assert.equal(bidA[5], buyerA, "Bid of buyerA is not properly saved");
+    assert.equal(bidB[5], buyerB, "Bid of buyerB is not properly saved");
+
+    await lwiico.removeReinforcedWhitelist([buyerA, buyerB],{from: whitelister}) 
+
+    increaseTime(100)
+
+    await expectThrow(lwiico.searchAndBid(1E18, 0, {from: buyerA, value:0.1E18}));
+    await expectThrow(lwiico.searchAndBid(1E18, 0, {from: buyerB, value:0.2E18}));
+  })  
+  
 });
